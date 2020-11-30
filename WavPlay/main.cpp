c621dbf75ba5b1b35da5a75560cae2e3f157227b
Bernard Helyer <b.helyer@gmail.com>
2020-11-30 20:14:51 +1300

    Play the sound!

--- WavPlay/main.cpp
+++ WavPlay/main.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
+#include <vector>
 #include <AL/AL.h>
 #include <AL/ALC.h>
+#define DR_WAV_IMPLEMENTATION
+#include "dr_wav.h"
 
 int main(int, char**) {
     // Initialise OpenAL device & context.
@@ -39,16 +42,40 @@ int main(int, char**) {
 
     // Open the WAV file.
 
-    FILE* fp = nullptr;
-    fopen_s(&fp, "C:/Code/Games/Rile/Rile/data/sound/MenuConfirm.wav", "rb");
-    if (fp == nullptr) {
-        fprintf(stderr, "Could not open file for reading.\n");
+    drwav wav;
+    if (!drwav_init_file(&wav, "C:/Code/Games/Rile/Rile/data/sound/MenuConfirm.wav", nullptr)) {
+        fprintf(stderr, "Couldn't load file for reading.\n");
         return 1;
     }
 
-    printf("Hey, I survived initialisation!\n");
+    // Read the PCM data, and then load that into the buffer.
 
-    fclose(fp);
+    std::vector<int16_t> sampleData;
+    sampleData.resize(wav.totalPCMFrameCount * wav.channels);
+    const auto framesRead = drwav_read_pcm_frames_s16(&wav, wav.totalPCMFrameCount, sampleData.data());
+    if (framesRead != wav.totalPCMFrameCount) {
+        fprintf(stderr, "Couldn't read file.\n");
+    }
+
+    printf("Channels: %d\n", wav.channels);
+    printf("Bits Per Sample: %d\n", wav.bitsPerSample);
+    printf("Sample Rate: %u\n", wav.sampleRate);
+
+    const ALenum format = wav.channels == 2 ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16;
+    alBufferData(buffer, format, sampleData.data(), static_cast<ALsizei>(sampleData.size() * sizeof(int16_t)), wav.sampleRate);
+    if (alGetError() != AL_NO_ERROR) {
+        fprintf(stderr, "Could not buffer data.\n");
+        return 1;
+    }
+
+    alSourcei(source, AL_BUFFER, buffer);
+    int sourceState;
+    alSourcePlay(source);
+    do {
+        alGetSourcei(source, AL_SOURCE_STATE, &sourceState);
+    } while (sourceState == AL_PLAYING);
+
+    drwav_uninit(&wav);
     alDeleteSources(1, &source);
     alDeleteBuffers(1, &buffer);
     alcDestroyContext(context);
